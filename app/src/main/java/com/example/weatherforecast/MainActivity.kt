1feb3c1219812707fdc5d8c34cbd45ada0e18aa8
sampleUser <one3piece9@gmail.com>
2025-11-21 21:52:25 +0900

    hiltの導入とHomeからWeatherへの画面遷移機能実装

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.weatherforecast.ui.theme.WeatherForecastTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/weatherforecast/ui/component/TopBar.kt
+++ app/src/main/java/com/example/weatherforecast/ui/component/TopBar.kt
@@ -1,23 +1,28 @@
 package com.example.weatherforecast.ui.component
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import com.example.weatherforecast.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(
-    @StringRes
-    titleResId: Int,
-    modifier: Modifier = Modifier
+    canNavigateBack: Boolean,
+    title: String,
+    modifier: Modifier = Modifier,
+    navigateUp: () -> Unit
 ) {
     Column {
         CenterAlignedTopAppBar(
@@ -26,9 +31,19 @@ fun TopBar(
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
-                        text = stringResource(titleResId),
+                        text = title,
                         style = MaterialTheme.typography.titleLarge
                     )
+                }
+            },
+            navigationIcon = {
+                if (canNavigateBack) {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back_button_description)
+                        )
+                    }
                 }
             },
             modifier = modifier
